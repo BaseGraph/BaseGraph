6227a18632aa55cc3a37b15a8f0c555b6dd444ed
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-03-29 15:57:50 -0400

    update documentation

--- include/BaseGraph/directedgraph.h
+++ include/BaseGraph/directedgraph.h
@@ -22,81 +22,108 @@ namespace BaseGraph{
 class DirectedGraph{
 
     public:
+        /// Construct DirectedGraph with \p size vertices.
+        /// @param size Number of vertices. Defaults to 0.
         explicit DirectedGraph(size_t size=0): size(0), edgeNumber(0) {resize(size);}
 
-        /// Change number of vertices. New size is required to be greater or equal to current size.
+        /// Change number of vertices.
+        /// @param size New number of vertices. Required to be greater or equal to current size.
         void resize(size_t size);
         /// Get number of vertices.
         size_t getSize() const { return size; }
         /// Get number of edges.
         size_t getEdgeNumber() const { return edgeNumber; }
 
-        /// Check if DirectedGraph have the same size and have the same edges.
+        /// Check if graph instance and \p other have the same size and have the same edges.
+        /// @param other DirectedGraph to compare to.
+        /// @return If graph instance is equal to \p other.
         bool operator==(const DirectedGraph& other) const;
-        /// Check if DirectedGraph have different sizes and/or have different edges.
+        /// Check if graph instance and \p other have different sizes and/or have different edges.
+        /// @param other DirectedGraph to compare to.
+        /// @return If graph instance is different from \p other.
         bool operator!=(const DirectedGraph& other) const { return !(this->operator==(other)); }
 
-        /** Add to graph the directed edge from vertex `source` to `destination`.
+        /** Add directed edge the from vertex \p source to \p destination.
+         * @param source, destination Indices of the source and destination vertices.
          * @param force If set to false, the edge is not added if it already exists. If set to true, the edge is always added, which can create multiedges.
-         *              Since these multiedges are not exepected, the methods and metrics computed on the graph might have unexepected behaviors. If one is
-         *              interested in using directed multigraphs, the class DirectedMultigraph is more appropriate.
+         *              Because this class doesn't handle multiedges, the methods and metrics computed on the graph might have unexepected behaviors. If one is
+         *              interested in using directed multigraphs, the class DirectedMultigraph is more appropriate. Defaults to false.
          */
-        void addEdgeIdx          (VertexIndex source, VertexIndex destination, bool force=false);
-        /// Add to graph the directed edge `edge`. Is equivalent to calling `addEdgeIdx` with the source and destination vertices.
-        void addEdgeIdx          (const Edge& edge, bool force=false) { addEdgeIdx(edge.first, edge.second, force); }
-        /// Add to graph two directed edges: from `vertex1` to `vertex2` and from `vertex2` to `vertex1`. Is equivalent to calling `addEdgeIdx` for both edge directions.
+        void addEdgeIdx(VertexIndex source, VertexIndex destination, bool force=false);
+        /// Equivalent to addEdgeIdx with vertices of \p edge.
+        void addEdgeIdx(const Edge& edge, bool force=false) { addEdgeIdx(edge.first, edge.second, force); }
+        /** Add reciprocal edges. Is equivalent to calling addEdgeIdx for both edge directions.
+         * @param vertex1, vertex2 Vertices of reciprocal edges.
+         * @param force If set to false, edges are not added if they already exist. If set to true, edges are always added, which can create multiedges.
+         *              Because this class doesn't handle multiedges, the methods and metrics computed on the graph might have unexepected behaviors. If one is
+         *              interested in using directed multigraphs, the class DirectedMultigraph is more appropriate. Defaults to false.
+         */
         void addReciprocalEdgeIdx(VertexIndex vertex1, VertexIndex vertex2, bool force=false) { addEdgeIdx(vertex1, vertex2, force); addEdgeIdx(vertex2, vertex1, force); }
-        /// Add to graph both orientations of `edge`.
+        /// Add reciprocal edges. Equivalent to addReciprocalEdgeIdx with vertices of \p edge.
         void addReciprocalEdgeIdx(const Edge& edge, bool force=false) {addReciprocalEdgeIdx(edge, force);}
-        /// Return if the directed edge from `source` to `destination` exists.
-        bool isEdgeIdx           (VertexIndex source, VertexIndex destination) const;
-        /// Return if the directed edge `edge` exists.
-        bool isEdgeIdx           (const Edge& edge) const { return isEdgeIdx(edge.first, edge.second); }
-        /// Remove all directed edges (including multiedges) starting from `source` going to `destination`.
-        void removeEdgeIdx       (VertexIndex source, VertexIndex destination);
-        /// Remove all directed edges (including multiedges) `edge`.
-        void removeEdgeIdx       (const Edge& edge) {removeEdgeIdx(edge.first, edge.second);}
-        /// Remove all multiedges.
+        /// Return if the directed edge from \p source to \p destination exists.
+        /// @param source, destination Indices of the source and destination vertices.
+        bool isEdgeIdx(VertexIndex source, VertexIndex destination) const;
+        /// Return if the directed edge \p edge exists. Equivalent to isEdgeIdx with vertices of \p edge.
+        bool isEdgeIdx(const Edge& edge) const { return isEdgeIdx(edge.first, edge.second); }
+        /// Remove all directed edges (including multiedges) from \p source to \p destination.
+        /// @param source, destination Indices of the source and destination vertices.
+        void removeEdgeIdx(VertexIndex source, VertexIndex destination);
+        /// Remove all directed edges (including multiedges) \p edge. Equivalent to removeEdgeIdx with vertices of \p edge.
+        void removeEdgeIdx(const Edge& edge) {removeEdgeIdx(edge.first, edge.second);}
+        /// Remove all multiedges. Multiedges can be created when using `force=true` when adding edges.
         void removeMultiedges();
         /// Remove all self-loops.
         void removeSelfLoops();
-        /// Remove all directed edges that contain `vertex`, which disconnects the vertex from the graph.
+        /// Remove all directed edges that contain `vertex`. It disconnects the vertex from the graph.
+        /// @param vertex Vertex to isolate in the graph.
         void removeVertexFromEdgeListIdx(VertexIndex vertex);
         /// Remove all edges.
         void clearEdges();
 
-        /// Construct a DirectedGraph that only contains the edges with vertices enumerated by the iterator. Returned graph has the same size as the caller object
-        /// such that the vertices keep the same indices.
+        /** Construct a DirectedGraph that only contains the edges with vertices enumerated by the iterator.
+         * @tparam Iterator Any iterator type that is valid in the construction of `std::unordered_set`.
+         * @param begin, end Iterators that enumerate vertices to preserve in subgraph.
+         * @return Directed subgraph without vertex remapping. The subgraph has the same number of vertices than the original graph.
+         */
         template <typename Iterator>
         DirectedGraph getSubgraphOfIdx(Iterator begin, Iterator end) const { return getSubgraphOfIdx(std::unordered_set<VertexIndex>(begin, end)); };
-        /// Construct a DirectedGraph that only contains the edges with `vertices`. Returned graph has the same size as the caller object
-        /// such that the vertices keep the same indices.
+        /** Construct a DirectedGraph that only contains the edges in \p vertices.
+         * @param vertices Vertices to include in the subgraph.
+         * @return Directed subgraph without vertex remapping. The subgraph has the same number of vertices than the original graph.
+         */
         DirectedGraph getSubgraphOfIdx(const std::unordered_set<VertexIndex>& vertices) const;
-        /// Construct a DirectedGraph that only contains the vertices enumerated by the iterator. The mapping of the original vertex indices to the subgraph vertex indices
-        /// is also returned.
+        /** Construct a DirectedGraph that only contains the edges with vertices enumerated by the iterator.
+         *  is also returned.
+         * @tparam Iterator Any iterator type that is valid in the construction of `std::unordered_set`.
+         * @param begin, end Iterators that enumerate vertices to preserve in subgraph.
+         * @return Directed subgraph and mapping of the original vertex indices to the subgraph vertex indices.
+         */
         template <typename Iterator>
         std::pair<DirectedGraph, std::unordered_map<VertexIndex, VertexIndex>> getSubgraphWithRemapOfIdx(Iterator begin, Iterator end) const {
             return getSubgraphWithRemapOfIdx(std::unordered_set<VertexIndex>(begin, end)); };
-        /// Construct a DirectedGraph that only contains `vertices`. The mapping of the original vertex indices to the subgraph vertex indices
-        /// is also returned.
+        /** Construct a DirectedGraph that only contains the edges in \p vertices.
+         * @param vertices Vertices to include in the subgraph.
+         * @return Directed subgraph and mapping of the original vertex indices to the subgraph vertex indices.
+         */
         std::pair<DirectedGraph, std::unordered_map<VertexIndex, VertexIndex>> getSubgraphWithRemapOfIdx(const std::unordered_set<VertexIndex>& vertices) const;
 
-        /// Get vertices to which `vertex` is connected to.
-        const Successors&   getOutEdgesOfIdx(VertexIndex vertex) const { assertVertexInRange(vertex); return adjacencyList[vertex]; }
+        /// Get vertices to which \p vertex is connected to.
+        const Successors& getOutEdgesOfIdx(VertexIndex vertex) const { assertVertexInRange(vertex); return adjacencyList[vertex]; }
         /// Get incoming neigbhours of each vertex.
-        AdjacencyLists      getInEdges() const;
+        AdjacencyLists getInEdges() const;
         /// Construct the adjacency matrix.
-        AdjacencyMatrix     getAdjacencyMatrix() const;
-        /// Count the number of incoming edges of `vertex`.
-        size_t              getInDegreeOfIdx(VertexIndex vertex) const;
+        AdjacencyMatrix getAdjacencyMatrix() const;
+        /// Count the number of incoming edges of \p vertex.
+        size_t getInDegreeOfIdx(VertexIndex vertex) const;
         /// Count the number of incoming edges of each vertex.
         std::vector<size_t> getInDegrees() const;
-        /// Count the number of outgoing edges starting from `vertex`.
-        size_t              getOutDegreeOfIdx(VertexIndex vertex) const;
+        /// Count the number of outgoing edges starting from \p vertex.
+        size_t getOutDegreeOfIdx(VertexIndex vertex) const;
         /// Count the number of outgoing edges of each vertex.
         std::vector<size_t> getOutDegrees() const;
 
-        /// Construct a DirectedGraph where each edge has its orientation reversed.
+        /// Construct a DirectedGraph where each directed edge is reversed.
         DirectedGraph getReversedGraph() const;
 
         /// Output in text format the graph's size and edges to a given `std::stream` object.
@@ -123,9 +150,8 @@ class DirectedGraph{
             iterator operator++() {++position; return *this;}
             iterator operator++(int) {iterator tmp=iterator(position); operator++(); return tmp;}
         };
-
-        iterator begin() const {return iterator(0);}
-        iterator end() const {return iterator(size);}
+        iterator begin() const { return iterator(0); }
+        iterator end() const { return iterator(size); }
 
     protected:
         AdjacencyLists adjacencyList;
