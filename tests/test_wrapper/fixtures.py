6e164333c80c6edd86bf1db6325e85b74b0bf143
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-05-19 13:02:53 -0400

    rework tests with networkx

---
+++ tests/test_wrapper/fixtures.py
@@ -0,0 +1,76 @@
+import pytest
+
+import networkx as nx
+import basegraph as bg
+
+
+filenames = {
+        "undirected": ["tests/assets/small_undirected_graph.txt", "tests/assets/undirected_graph.txt"],
+        "directed": ["tests/assets/small_directed_graph.txt", "tests/assets/directed_graph.txt"]
+    }
+small_graph_threshold = 100
+
+
+def get_graphs(filename, directed):
+    if not directed:
+        return bg.io.load_undirected_text_edgelist(filename), nx.read_edgelist(filename)
+
+    bg_graph = bg.io.load_directed_text_edgelist(filename)
+    bg_graph.remove_self_loops()
+
+    nx_graph = nx.read_edgelist(filename, create_using=nx.DiGraph)
+    nx_graph.remove_edges_from(nx.selfloop_edges(nx_graph))
+
+    return bg_graph, nx_graph
+
+
+@pytest.fixture
+def directed_fixture():
+    return [get_graphs(filename, directed=True) for filename in filenames["directed"]]
+
+
+@pytest.fixture
+def small_directed_fixture():
+    graphs = [get_graphs(filename, directed=True) for filename in filenames["directed"]]
+    return list(filter(lambda graph: graph[0].get_size()<small_graph_threshold, graphs))
+
+
+@pytest.fixture
+def undirected_fixture():
+    return [get_graphs(filename, directed=False) for filename in filenames["undirected"]]
+
+
+@pytest.fixture
+def small_undirected_fixture():
+    graphs = [get_graphs(filename, directed=False) for filename in filenames["undirected"]]
+    return list(filter(lambda graph: graph[0].get_size()<small_graph_threshold, graphs))
+
+
+def eq(graphs, bg_metric, nx_metric):
+    for bg_graph, nx_graph in graphs:
+        assert bg_metric(bg_graph) == nx_metric(nx_graph)
+
+
+def approx(graphs, bg_metric, nx_metric):
+    for bg_graph, nx_graph in graphs:
+        assert pytest.approx(bg_metric(bg_graph)) == nx_metric(nx_graph)
+
+
+def eq_all_vertices(graphs, bg_metric, nx_metric):
+    for bg_graph, nx_graph in graphs:
+        bg_values = bg_metric(bg_graph)
+        nx_values = nx_metric(nx_graph)
+
+        print(bg_values)
+        print(nx_values)
+        for i, vertex in enumerate(bg_graph.get_vertices()):
+            assert bg_values[i] == nx_values[vertex]
+
+
+def approx_all_vertices(graphs, bg_metric, nx_metric):
+    for bg_graph, nx_graph in graphs:
+        bg_values = bg_metric(bg_graph)
+        nx_values = nx_metric(nx_graph)
+        for i, vertex in enumerate(bg_graph.get_vertices()):
+            print(vertex, ":",  bg_values[i], nx_values[vertex])
+            assert pytest.approx(bg_values[i]) == nx_values[vertex]
--- tests/test_wrapper/networkx_additional_metrics.py
+++ tests/test_wrapper/networkx_additional_metrics.py
@@ -4,92 +4,98 @@ from networkx.algorithms.shortest_paths.generic import shortest_path
 import numpy as np
 
 
-def get_networkx_shortestpaths_handler(values, default):
-    def f(key):
-        try:
-            return values[key]
-        except KeyError:
-            return default
-    return f
+def get_diameters(graph):
+    if graph.is_directed():
+        subgraphs = [graph.subgraph(component).copy() for component in nx.strongly_connected_components(graph)]
+    else:
+        subgraphs = [graph.subgraph(component).copy() for component in nx.connected_components(graph)]
+    print("component number", len(subgraphs))
+
+    diameters = {}
+    for component in subgraphs:
+        diameters.update(nx.algorithms.distance_measures.eccentricity(component))
+    return diameters
 
 
-def get_shortest_paths(graph, vertex_labels):
+def get_redundancy(graph):
+    degrees = graph.degree
+    redundancy = nx.algorithms.cluster.clustering(graph)
+
+    for vertex in redundancy:
+        redundancy[vertex] *= degrees[vertex]-1
+    return redundancy
+
+
+def get_sorted_triangles(graph):
+    return [clique for clique in nx.enumerate_all_cliques(graph) if len(clique)==3]
+
+
+def get_shortest_path_lengths(graph):
     n = graph.number_of_nodes()
 
-    shortest_paths = []
-    for vertex_label in vertex_labels:
-        distances = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex_label)
-        ordered_distances = list(map(get_networkx_shortestpaths_handler(distances, -1), vertex_labels))
-        shortest_paths.append(ordered_distances)
+    shortest_paths = {}
+    for vertex in graph:
+        shortest_paths[vertex] = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex)
 
     return shortest_paths
 
 
-def get_shortest_path_averages(graph, vertex_labels):
-    n = graph.number_of_nodes()
-
-    averages = []
-    for vertex_label in vertex_labels:
-        distances = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex_label)
-        nonzero_distances = list(filter( lambda x: 0<x<n, distances.values() ))
-
-        if nonzero_distances == []:
-            averages.append(0)
-        else:
-            averages.append(np.mean(nonzero_distances))
+def get_shortest_path_averages(graph):
+    averages = {}
+    for vertex in graph:
+        distances = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex)
+        del distances[vertex]
+        averages[vertex] = 0 if not distances else np.mean(list(distances.values()))
 
     return averages
 
 
-def get_shortest_path_harmonic_averages(graph, vertex_labels):
-    n = graph.number_of_nodes()
+def get_shortest_path_harmonic_averages(graph):
+    averages = {}
+    for vertex in graph:
+        distances = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex)
+        del distances[vertex]
+        path_lengths = list(distances.values())
+        averages[vertex] = 0 if not distances else\
+                    sum(map(lambda x: 1/x, path_lengths))/len(path_lengths)
 
-    harmonic_averages = []
-    for vertex_label in vertex_labels:
-        distances = nx.algorithms.shortest_paths.generic.shortest_path_length(graph, source=vertex_label)
-        nonzero_distances = list(filter( lambda x: 0<x<n, distances.values() ))
-
-        if nonzero_distances == []:
-            harmonic_averages.append(0)
-        else:
-            harmonic_averages.append(sum(map(lambda x: 1/x, nonzero_distances))/len(nonzero_distances))
-
-    return harmonic_averages
+    return averages
 
 
-def get_neighbourhood_degree_spectra(graph, vertex_labels):
+def get_neighbourhood_degree_spectra(graph):
     degrees = graph.degree
 
-    neighbourhood_degree_spectra = []
-    for vertex_label in vertex_labels:
-        neighbour_degrees = [degrees[neighbour] for neighbour in graph.neighbors(vertex_label)]
+    neighbourhood_degree_spectra = {}
+    for vertex in graph:
+        neighbour_degrees = [degrees[neighbour] for neighbour in graph.neighbors(vertex)]
 
         hist = dict(Counter(neighbour_degrees))
         for key in hist:
-            hist[key] /= degrees[vertex_label]
-        neighbourhood_degree_spectra.append(hist)
+            hist[key] /= degrees[vertex]
+        neighbourhood_degree_spectra[vertex] = hist
 
     return neighbourhood_degree_spectra
 
 
-def get_onion_spectrum(graph, vertices):
-    layer = nx.algorithms.core.onion_layers(graph)
+def get_onion_spectrum(graph):
+    onion_layers = nx.algorithms.core.onion_layers(graph)
     kcore = nx.algorithms.core.core_number(graph)
+    n = graph.number_of_nodes()
 
-    nb_layers = max(layer.values())
+    nb_layers = max(onion_layers.values())
     odlayer2kshell = np.zeros(nb_layers + 1)
     onion_spectrum = np.zeros(nb_layers + 1)
 
-    for n in layer:
-        odlayer2kshell[ layer[n] ] = kcore[n]
-        onion_spectrum[ layer[n] ] += 1
+    for vertex in onion_layers:
+        odlayer2kshell[ onion_layers[vertex] ] = kcore[vertex]
+        onion_spectrum[ onion_layers[vertex] ] += 1
 
     onion_spect = {}
 
-    for o in range(1, len(onion_spectrum)):
-        if odlayer2kshell[o-1] < odlayer2kshell[o]:
-          onion_spect[ odlayer2kshell[o] ] = []
-        onion_spect[ odlayer2kshell[o] ].append(onion_spectrum[o] / graph.number_of_nodes() )
+    for layer in range(1, len(onion_spectrum)):
+        if odlayer2kshell[layer-1] < odlayer2kshell[layer]:
+          onion_spect[ odlayer2kshell[layer] ] = []
+        onion_spect[ odlayer2kshell[layer] ].append(onion_spectrum[layer]/n)
 
     return onion_spect
 
@@ -101,9 +107,11 @@ def get_clustering_spectrum(graph):
     d = [deg[n] for n in graph.nodes()]
     c = [clu[n] for n in graph.nodes()]
 
-    spectrum = {k : x / n for x,k,n in zip(
-                 np.histogram(d, bins=np.arange(0.5, np.max(d)+1.5, 1), weights=c)[0],
-                 np.arange(1, np.max(d)+2, 1),
-                 np.histogram(d, bins=np.arange(0.5, np.max(d)+1.5, 1))[0]) if n > 0}
+    spectrum = {k : x/n for x, k, n in zip(
+                     np.histogram(d, bins=np.arange(0.5, np.max(d)+1.5, 1), weights=c)[0],
+                     np.arange(1, np.max(d)+2, 1),
+                     np.histogram(d, bins=np.arange(0.5, np.max(d)+1.5, 1))[0])
+                 if n > 0
+               }
     del spectrum[1]
     return spectrum
