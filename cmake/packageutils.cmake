c72f45ec1f99f8f1197830b2353cf615a9d9527e
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-05-25 23:28:48 -0400

    move cmake basegraph config into cmake/, remove unused function, set version in cmake package

--- cmake/packageutils.cmake
+++ cmake/packageutils.cmake
@@ -20,28 +20,3 @@ macro(add_extension _extension)
     list(APPEND BaseGraphExtensions ${_extension})
     set(BaseGraphExtensions ${BaseGraphExtensions} PARENT_SCOPE)
 endmacro()
-
-
-include(CMakePackageConfigHelpers)
-include(GNUInstallDirs)
-
-# Snippet taken from https://stackoverflow.com/questions/33462209/making-a-cmake-library-accessible-by-other-cmake-packages-automatically
-function(export_target _target _include_dir)
-    file(
-        WRITE "${CMAKE_CURRENT_BINARY_DIR}/${_target}Config.cmake"
-        "
-            include(\"\$\{CMAKE_CURRENT_LIST_DIR\}/${_target}Targets.cmake\")
-            set_property(
-                TARGET ${_target}
-                APPEND PROPERTY
-                    INTERFACE_INCLUDE_DIRECTORIES \"${_include_dir}\"
-            )
-        "
-    )
-
-    export(TARGETS ${_target} FILE "${CMAKE_CURRENT_BINARY_DIR}/${_target}Targets.cmake")
-
-    # NOTE: The following call can pollute your PC's CMake package registry
-    #       See comments/alternatives below
-    export(PACKAGE ${_target})
-endfunction(export_target)
