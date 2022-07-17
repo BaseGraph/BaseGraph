288984cd5d9f66fcdd012b8c1574e2448d421076
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-07-17 15:15:34 -0400

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages
 
 setup(
     name='basegraph',
-    version='0.1.0',
+    version='0.2.0',
     author='Simon Lizotte',
     description='A light-weight, efficient and expressive graph library.',
     license="GPLv3",
