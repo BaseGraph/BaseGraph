b32327654d09c5c2d5fb10b3d422757b99a6f98d
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-09-08 11:23:30 -0400

    move back __init__.py into root to fix python import path

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description='A light-weight, efficient and expressive graph library.',
     license="GPLv3",
     zip_safe=False,
-    packages=["pybind_wrapper/basegraph"],
+    packages=["basegraph"],
     cmake_args=["-DBUILD_BINDINGS=ON"],
     include_package_data=True,
     exclude_package_data={'': ["__pycache__"]},
